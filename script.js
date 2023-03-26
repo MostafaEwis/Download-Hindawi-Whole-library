5b8e9e25392386ea4a7832db8f400692ebb09930
Mostafa Ewis <64862002+MostafaEwis@users.noreply.github.com>
2023-03-27 01:20:42 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const { Builder, By } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+// حط الـ file path  هنا يا عموري
+const service = new chrome.ServiceBuilder(
+  "C:/Users/mosta/Downloads/Drivers/chromedriver.exe"
+);
+const driver = new Builder()
+  .forBrowser("chrome")
+  .setChromeService(service)
+  .build();
+const happen = async () => {
+  driver.get("https://www.hindawi.org/books");
+  await driver.manage().setTimeouts({ implicit: 5000 });
+  // اللوب دي بتلف على صفحات الكتب كلها جبت عددهم من هنداوي
+  for (let i = 0; i < 149; i++) {
+    // دي لستة الكتب
+    let books = await driver.findElements(By.className("link"));
+    // ده الزرار اللي بينقلنا لصفحة الكتب الجاية
+    let goNext = await driver.findElements(By.className("mIcon"));
+    //  دول بيخلوا الـ terminal  شكله روش ولكن ملهمش لازم الحقيقة
+    console.table(goNext);
+    console.table(books);
+    // اللوب دي بتلف على الكتب في الصفحة عشان تنزلهم
+    for (let i = 0; i < books.length; i++) {
+      const book = books[i];
+      // كل كتاب هنا بنخش عليه وبنحمله pdf
+      await book.click();
+      await driver.findElement(By.id("pdf")).click();
+      // بنرجع للصفحة الرئيسية عشان نفتح كتاب تاني
+      await driver.navigate().back();
+    }
+    // بنروج الصفحة اللي بعدها من هنا
+    await goNext[5].click();
+  }
+};
+
+happen();
